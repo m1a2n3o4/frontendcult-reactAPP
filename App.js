7abda086912a5c4ec032ca1101012303a62f8b73
m1a2n3o4 <manumano111@gmai.com>
2023-08-16 09:03:58 +0530

    New files

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-
+import React  from "react";
+import ReactDOM  from "react-dom";
 
 const h1tag = React.createElement("div",{ id : "parent"},
 React.createElement("div",{ id : "child"}),
-React.createElement("h1",{ id : "some"}, "Hello All, I am from Reactjs and NestedH1")
+React.createElement("h1",{ id : "some"}, "Hello All manu some , I am from Reactjs and NestedH1")
 );
 
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# React_app
+
+# to run the app run
+# npm install
+# npx parcel index.html
+
+
+# Parecl
+- Dev Build.
+- Localr Server
+- HMR - HOT module replacement.
+- Cahche 
+- Image optimaization.
+- Minification.
+- code spliting
+- Error Handeling.
+- Hosting on HTTPS.
+- Diffrent biundels like Dev and PROD.
