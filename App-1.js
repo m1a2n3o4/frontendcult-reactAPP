2807346698332f1fdf6aa601487691e51a7b6b79
m1a2n3o4 <manumano111@gmai.com>
2023-08-18 09:42:05 +0530

    Props, Multi compoments - Ref: Swiggy

---
+++ App-1.js
@@ -0,0 +1,59 @@
+import React  from "react";
+import ReactDOM  from "react-dom";
+
+// React Create Element
+const h1tag = React.createElement("div",{ id : "parent"}, "some tetxt");
+
+//JSX - 5%
+// Babel - 
+let name =" manohar";
+let mobile = "9553345235"
+const jsXdivision = <div id="myid" className="my-div"><p>My name is { name } and my mobile number is { mobile }</p>
+<p id="name">dSDfasdfasdf</p>JSX
+</div>;
+
+// React Components
+
+// Everything in react components. 2 ways) 
+ // 1) Class based components  (OLD Way)
+ // 2) Functional/methods based components (99.999 )
+ // ....................................Components.........................
+   const userDetails = {
+    usernmar: "Manohar",
+    mobile : 9553345235
+   }
+ const ButtonCOM = () => (
+    <input type="text"/>
+ );
+   const HeaderComponent = () => (
+    <div class="header">
+      <h2>{ userDetails.usernmar}, What's on your mind</h2></div>
+   );
+
+   const ContentComponent = () => (
+    <div class="content">
+        <h2>The user name :  { userDetails.usernmar }</h2>
+    </div>
+   )
+
+   const FooterComponent = () => (
+    <div class="footer"><h2>my content</h2></div>
+   )
+
+const HomepageComponent = () => (
+    <div className="container">
+        <HeaderComponent></HeaderComponent>
+        <ContentComponent></ContentComponent>
+        <ContentComponent></ContentComponent>
+        <FooterComponent></FooterComponent>
+    </div>
+);
+   
+
+
+console.log(h1tag);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HomepageComponent/>); //Rectjs element - HTML Element
+
--- App.js
+++ App.js
@@ -1,57 +1,115 @@
 import React  from "react";
 import ReactDOM  from "react-dom";
 
-// React Create Element
-const h1tag = React.createElement("div",{ id : "parent"}, "some tetxt");
+/**
+ * HeaderComponent
+ *  -Logo
+ *  - Menu Iteam
+ * BodyComponent
+ *  -SearchBar
+ *  -RestoComponent
+ *    - Image
+ *    - Resto Name
+ *    - Rating.
+ *    - Devlivery Time
+ * FooterComponent
+ *  - Copyrights.
+ *  - Links, 
+ *  Properties for components.
+ */
 
-//JSX - 5%
-// Babel - 
-let name =" manohar";
-let mobile = "9553345235"
-const jsXdivision = <div id="myid" className="my-div"><p>My name is { name } and my mobile number is { mobile }</p>
-<p id="name">dSDfasdfasdf</p>JSX
-</div>;
-
-// React Components
-
-// Everything in react components. 2 ways) 
- // 1) Class based components  (OLD Way)
- // 2) Functional/methods based components (99.999 )
- // ....................................Components.........................
-   const userDetails = {
-    usernmar: "Manohar",
-    mobile : 9553345235
-   }
- const ButtonCOM = () => (
-    <input type="text"/>
- );
-   const HeaderComponent = () => (
-    <div class="header"><h2>my Header</h2><ButtonCOM/></div>
-   );
-
-   const ContentComponent = () => (
-    <div class="content">
-        <h2>The user name :  { userDetails.usernmar }</h2>
-    </div>
-   )
-
-   const FooterComponent = () => (
-    <div class="footer"><h2>my content</h2></div>
-   )
-
-const HomepageComponent = () => (
-    <div className="container">
-        <HeaderComponent></HeaderComponent>
-        <ContentComponent></ContentComponent>
-        <FooterComponent></FooterComponent>
-    </div>
-);
-   
+const HeaderComponent = () => {
+    return (
+      <div className="header">
+         <div className="logo">
+            <img src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg?w=100" alt="log"></img>
+         </div>
+         <div className="navitem">
+            <ul>
+                <li>Home</li>
+                <li>About US</li>
+                <li>Services</li>
+                <li>Top offers</li>
+                <li>My card</li>
+            </ul>
+         </div>
+      </div>
+    )
+};
 
 
-console.log(h1tag);
+const RestoComponent = (props) => {
+    console.log(props);
+    return (
+        <div className="resto">
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/3632e911860bd3a90f9558fc81372b73" alt=""/>
+            <h2>{props.resName}</h2>
+            <p>{props.resMake}</p>
+            <h4>{props.resRating}</h4>
+            <h4>{props.resDeliverytime}</h4>
+        </div>
+    )
+};
+
+const BodyComponent = () => {
+    return (
+        <div className="body">
+            <div className="resto-container">
+            <RestoComponent 
+            resName = "VaraLashmi" 
+            resRating = "3.6" 
+            resDeliverytime="30min" 
+            resMake="Birani, Tandoori"
+            />
+            <RestoComponent
+             resName = "KFC - food" 
+             resRating = "3.8" 
+             resDeliverytime="40min" 
+             resMake="Fried pices"/>
+            <RestoComponent
+             resName = "Manoharaaaaaaa food" 
+             resRating = "5" 
+             resDeliverytime="1min" 
+             resMake="All indian foods"/>
+            <RestoComponent
+             resName = "VaraLashmi 2" 
+             resRating = "4" 
+             resDeliverytime="20min" 
+             resMake="Birani, Tandoori"/>
+              <RestoComponent
+             resName = "VaraLashmi 2" 
+             resRating = "4" 
+             resDeliverytime="20min" 
+             resMake="Birani, Tandoori"/>
+            </div>
+            
+        </div>
+    )
+};
+
+
+const FooterComponent = () => {
+    return (
+        <div className="footer"><h2>Footer component</h2></div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+         <div className="container-fluid">
+            <HeaderComponent></HeaderComponent>
+            <BodyComponent></BodyComponent>
+            <FooterComponent></FooterComponent>
+         </div>
+    )
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<HomepageComponent/>); //Rectjs element - HTML Element
+root.render(<AppLayout></AppLayout>); 
 
+function funname (a, b, c) {
+    console.log(a);
+}
+
+funname(1,2,3);
