2a43eee3365a0347ef538300da6a15d9e009f4ba
m1a2n3o4 <manumano111@gmai.com>
2023-08-22 08:59:49 +0530

    new changes

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const FooterComponent = () => {
+    return (
+        <div className="footer"><h2>Footer component</h2></div>
+    )
+}
+
+export default FooterComponent;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const HeaderComponent = () => {
+    return (
+      <div className="header">
+         <div className="logo">
+            <img src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg?w=100" alt="log"></img>
+         </div>
+         <div className="navitem">
+            <ul>
+                <li>Home</li>
+                <li>About US</li>
+                <li>Services</li>
+                <li>Top offers</li>
+                <li>My card</li>
+            </ul>
+         </div>
+      </div>
+    )
+}
+
+export default HeaderComponent;
